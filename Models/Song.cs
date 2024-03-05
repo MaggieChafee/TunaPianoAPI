ba2fca9e19876c4bb6a8ca404f0be67d69541467
MaggieChafee <maggie.chafee@gmail.com>
2024-03-04 19:25:14 -0600

    Initial Setup

---
+++ Models/Song.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TunaPianoAPI.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public string Album {  get; set; }
+        public int Length { get; set; }
+    }
+}
